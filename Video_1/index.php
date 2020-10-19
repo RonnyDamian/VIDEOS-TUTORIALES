04b6e93651533d1d8d73d202338202e55bf52d1c
DaamianRonny <FernandaSuaresAstudillo@gmail.com>
2020-10-18 20:16:07 -0500

    Videos de la semana 2

---
+++ Video_1/index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vide 1</title>
+    <link rel="shortcut icon" href="images/joint1.png" type="image/x-icon" style="border-radius: 50%">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fixed.css">
+
+</head>
+<body data-spy="scroll" data-target="#navbarResponsive">
+
+<!--Start Home Section -->
+<div id="home">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top ">
+    <a href="#home" class="navbar-brand">
+        <img src="images/joint3.png" alt="Jointbs S.A.S">
+    </a>
+    <button class="navbar-toggler" type="button"  data-toggle="collapse"  data-target="#navbarResponsive">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="#home" class="nav-link">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#course" class="nav-link">Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a href="#features" class="nav-link">Características</a>
+            </li>
+            <li class="nav item">
+                <a href="#resources" class="nav-link">Recursos</a>
+            </li>
+            <li class="nav-item">
+                <a href="#clients" class="nav-link">Clientes</a>
+            </li>
+            <li class="nav-item">
+                <a href="#contact" class="nav-link">Contacto</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--End Home Section -->
+<!--Start Landing Page Section-->
+
+<!--End Landing Page Section  -->
+
+<!--Start Cursos Section-->
+<div id="course" class="offset">
+
+</div>
+<!--End Course Section-->
+
+<!--Start Cursos Section-->
+    <div id="" class="offset">
+
+</div>
+<!--End Course Section-->
+
+<!--Start Features Section-->
+<div id="features" class="offset">
+
+</div>
+<!--End Features Section-->
+
+<!--Start Resources Section-->
+<div id="resources" class="offset">
+
+</div>
+<!--End Resorces Section-->
+
+<!--Start Clientes Section-->
+<div id="clients" class="offset">
+
+</div>
+<!--End Clientes Section-->
+
+
+<!--Start Contact Section-->
+<div id="contact" class="offset">
+
+</div>
+<!--End Contact  Section-->
+
+
+
+
+
+
+<!--Script sources files -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="fontAwesome/fontAwesome.js"></script>
+<!--End of script source files -->
+</body>
+</html>
